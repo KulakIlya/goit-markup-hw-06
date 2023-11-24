b1263c12a26dc4ccd98a105a4790251c9cb97718
Ilya <kulak1224@gmail.com>
2023-11-24 17:20:42 +0200

    Adding burger menu and responsive modal window

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const heroBtn = document.querySelector('.hero-btn');
-const modalWindow = document.querySelector('.backdrop');
+const burgerBtn = document.querySelector('.burger-btn');
 
-heroBtn.addEventListener('click', toggleModal);
+const modalWindow = document.querySelector('.backdrop');
+const mobileNav = document.querySelector('.mobile-nav-backdrop');
+
+heroBtn.addEventListener('click', () => toggleModal(modalWindow));
+burgerBtn.addEventListener('click', () => toggleModal(mobileNav));
 
 modalWindow.addEventListener('click', (e) => {
   const target = e.target.closest('.btn-close');
   if (!target) return;
 
-  toggleModal();
+  toggleModal(e.currentTarget);
 });
 
-function toggleModal() {
-  modalWindow.classList.toggle('is-open');
+mobileNav.addEventListener('click', (e) => {
+  const target = e.target.closest('.btn-close');
+  if (!target) return;
+
+  toggleModal(e.currentTarget);
+});
+
+function toggleModal(ref) {
+  ref.classList.toggle('is-opened');
 }
